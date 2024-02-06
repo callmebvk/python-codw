d09928c9fcffdfdee5aff99c718db6b869d47c0e
callmebvk <balirajekalyane01@gmail.com>
2024-02-06 09:56:55 +0530

    frist commit

---
+++ basin_info_filling.py
@@ -0,0 +1,6 @@
+name = "baliraje kalyane"
+age = 23
+work = "student"
+branch="engineering in artificial integellion branch ."
+
+print(f"my name is {name}, my age is {age}. i am {work} and study in {branch}")
---
+++ main.py
@@ -0,0 +1,9 @@
+def replace_word():
+
+    str = "hi guys , i am baliraje kalyane and hi hi hi "
+    word_to_replace = input("enter the word to replace: ")
+    word_replacement = input("enter to word replacement: ")
+    print(str.replace(word_to_replace,word_replacement))
+replace_word()
+
+    
